5854c76808d0547a9ddc2f2cea7d3535d5f16d6e
mdxst <mirkoemmebi@duck.com>
2025-12-26 21:03:43 +0100

    Aggiunta più varietà ai test, di cui ora ogni funzione cambia il livello max

--- golog_test.go
+++ golog_test.go
@@ -6,24 +6,32 @@ import (
 )
 
 func TestLog(t *testing.T) {
+	LivelloMax = 0
+	t.Logf("impostato LivelloMax a 0.")
 	log := Log("DEBUG", "test", 0)
 	t.Logf("output di Log(\"DEBUG\", \"test\", 0) era %v", log)
 	// t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 }
 
 func TestLogVuoto(t *testing.T) {
+	LivelloMax = -1
+	t.Logf("impostato LivelloMax a -1.")
 	//t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	log := Log("", "", 0)
 	t.Logf("output di Log(\"\", \"\", 0) era %v", log)
 }
 
 func TestLogE(t *testing.T) {
+	LivelloMax = 571
+	t.Logf("impostato LivelloMax a 571.")
 	//t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	log := LogE(errors.New("test a a a errore finto!"))
 	t.Logf("output di LogE(err) era %v", log)
 }
 
 func TestLogW(t *testing.T) {
+	LivelloMax = 2
+	t.Logf("impostato LivelloMax a 2.")
 	//t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	log := LogW("test", "test1", "dovrebbe", "essere", "staccato!")
 	t.Logf("output di LogW(\"test\") era %v", log)
@@ -39,8 +47,8 @@ func TestLogI(t *testing.T) {
 }
 
 func TestLogD(t *testing.T) {
-	LivelloMax = 3
-	t.Logf("impostato LivelloMax a 3.")
+	LivelloMax = 6
+	t.Logf("impostato LivelloMax a 6.")
 	//t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	log := LogD("test", "test1", "dovrebbe", "essere", "staccato!")
 	t.Logf("output di LogW(\"test\") era %v", log)
